402c14c2ca32ad2e167e83eff285e927e5a1b057
Diana Sosnovskaya <progdidi@gmail.com>
2024-02-04 02:02:14 +0300

    fix: fix mainpage structure

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,4 +1,5 @@
 import './mainPage.scss';
+import example from '../../images/example.jpg';
 
 const MainPage = () => {
     return ( 
@@ -7,9 +8,14 @@ const MainPage = () => {
             <div className="container">
                 <div className="promo__inner">
                     <div className="promo__info">
-                        <h1 className="promo__title">Books store</h1>
+                        <h1 className="promo__title">The Library - everything about books</h1>
+                        <p className="promo__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem enim molestiae molestias corporis deleniti sequi incidunt eligendi voluptas harum reiciendis officia voluptate, recusandae praesentium dolorem mollitia? Tempore, non laudantium!</p>
                     </div>
-                    <div className="promo__media"></div>
+                    <div className="promo__media">
+                        <div className="promo__slider">
+                            <img src={example} alt="" className="promo__slider-img" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
