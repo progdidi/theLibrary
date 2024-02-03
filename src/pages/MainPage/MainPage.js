6e42a079dae107d59eafedb7327592d313c1bc9f
Diana Sosnovskaya <progdidi@gmail.com>
2024-02-04 01:37:38 +0300

    feat: add main page

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,20 @@
+import './mainPage.scss';
+
+const MainPage = () => {
+    return ( 
+        <>
+        <section className="promo">
+            <div className="container">
+                <div className="promo__inner">
+                    <div className="promo__info">
+                        <h1 className="promo__title">Books store</h1>
+                    </div>
+                    <div className="promo__media"></div>
+                </div>
+            </div>
+        </section>
+        </>
+     );
+}
+ 
+export default MainPage;
